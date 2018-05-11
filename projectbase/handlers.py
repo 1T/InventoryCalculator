af8a6457fcf1523f42361e807c8c4a4bfcc34a9c
Nipun Iyer <nipun@1ticket.com>
2018-05-11 13:03:01 -0400

    use new projectbase and log at most 5 keys

--- projectbase/handlers.py
+++ projectbase/handlers.py
@@ -1,7 +1,7 @@
 from logging import getLogger
 from logging.config import dictConfig
 from typing import Dict, Any
-
+from json import dumps
 
 from projectbase.settings import LOGGING_CONFIG
 
@@ -13,7 +13,7 @@ dictConfig(LOGGING_CONFIG)
 def example_get(event: Dict[str, Any], _: Any) -> None:
     """Handle example get request."""
     # log dictionaries directly: adding key to root level override other fie
-    _logger.info(event)
+    _logger.info(f"Event: {dumps(event)}")
     # Or, log message and extra dictionary
     _logger.info("mymessage", extra={"userid": 1.23, "itemid": 0})
     return {
@@ -23,7 +23,14 @@ def example_get(event: Dict[str, Any], _: Any) -> None:
 
 def example_post(event: Dict[str, Any], _: Any) -> None:
     """Handle example get request."""
-    _logger.info('handle post', extra=event)
+    _logger.info(f'handle post: {dumps(event)}')
+    extra_dict = {
+        'log': True,
+        'most': True,
+        'five': True,
+        'keys': True
+    }
+    _logger.info(f'my_dict', extra=extra_dict)
     return {
         'event': event
     }
