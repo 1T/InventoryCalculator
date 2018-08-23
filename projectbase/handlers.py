4a2fcc340214a2faed223807c07cb39fbadd9e27
Justin Mayer <jmayer14@gmail.com>
2018-08-23 15:40:49 -0400

    Add Kinesis to ES logging

--- projectbase/handlers.py
+++ projectbase/handlers.py
@@ -1,35 +1,26 @@
 from logging import getLogger
+from logging.config import dictConfig
 from typing import Dict, Any
-from json import dumps
 
-from projectbase.settings import LOGGING_CONFIG
-from projectbase.log import init_logging
+from kinesislogger import LOGGING_CONFIG
+
 
 _logger = getLogger(__name__)
-init_logging()
+dictConfig(LOGGING_CONFIG)
 
 
-def example_get(event: Dict[str, Any], _: Any) -> None:
+def example_get(event: Dict[str, Any], _: Any) -> Dict:
     """Handle example get request."""
-    # log dictionaries directly: adding key to root level override other fie
-    _logger.info(f"Event: {dumps(event)}")
-    # Or, log message and extra dictionary
-    _logger.info("mymessage", extra={"userid": 1.23, "itemid": 0})
+    for i in range(100):
+        _logger.info(event)
     return {
         'event': event
     }
 
 
-def example_post(event: Dict[str, Any], _: Any) -> None:
-    """Handle example get request."""
-    _logger.info(f'handle post: {dumps(event)}')
-    extra_dict = {
-        'logurl': True,
-        'jobid': True,
-        'listingid': True,
-        'userid': True
-    }
-    _logger.info(f'my_dict', extra=extra_dict)
+def example_post(event: Dict[str, Any], _: Any) -> Dict:
+    """Handle example post request."""
+    _logger.info(event)
     return {
         'event': event
     }
