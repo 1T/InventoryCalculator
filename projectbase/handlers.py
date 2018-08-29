749679e3d010a71e6d72a59b8148e1521b8f3273
Justin Mayer <jmayer14@gmail.com>
2018-08-29 15:34:22 -0400

    Update kinesislogger

--- projectbase/handlers.py
+++ projectbase/handlers.py
@@ -1,18 +1,15 @@
-from logging import getLogger
-from logging.config import dictConfig
 from typing import Dict, Any
 
-from kinesislogger import LOGGING_CONFIG
+from kinesislogger import get_logger
 
 
-_logger = getLogger(__name__)
-dictConfig(LOGGING_CONFIG)
+_logger = get_logger()
 
 
 def example_get(event: Dict[str, Any], _: Any) -> Dict:
     """Handle example get request."""
     for i in range(100):
-        _logger.info(event)
+        _logger.info(event, extra={'userid': 'lolol'})
     return {
         'event': event
     }
