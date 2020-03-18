bddcfc650ccd8e81bca9441987e058101a84a325
Valentin Druzhinin <valentindruzhinin@Valentins-MacBook-Pro.local>
2020-03-18 14:35:43 +0200

    Add logging

--- inventorycalculator/core/storages/s3_storage.py
+++ inventorycalculator/core/storages/s3_storage.py
@@ -1,6 +1,10 @@
 import boto3
 from botocore.exceptions import ClientError
 from inventorycalculator.errors import S3StorageError
+from OneTicketLogging import elasticsearch_logger
+
+
+_logger = elasticsearch_logger(__name__)
 
 
 class S3Storage:
@@ -16,6 +20,7 @@ class S3Storage:
                 Bucket=self._bucket_name
             )
         except ClientError as e:
+            _logger.error(e)
             raise S3StorageError('Unable to upload given data')
 
     def get(self, key: str) -> str:
@@ -25,4 +30,5 @@ class S3Storage:
                 Bucket=self._bucket_name
             )['Body'].read().decode('utf-8')
         except ClientError as e:
+            _logger.error(e)
             raise S3StorageError(f'Resource not exists by given key:{key}')
