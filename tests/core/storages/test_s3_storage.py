f7e2cbee8a151f55eb520bd1ead09b8e04a3e135
Valentin Druzhinin <valentindruzhinin@Valentins-MacBook-Pro.local>
2020-03-18 11:56:56 +0200

    Add S3Storage.get method

--- tests/core/storages/test_s3_storage.py
+++ tests/core/storages/test_s3_storage.py
@@ -4,6 +4,8 @@ from unittest.mock import patch, MagicMock, Mock
 from inventorycalculator.errors import S3StorageError
 from botocore.exceptions import ClientError
 
+from tests.mocks import MockS3Body
+
 
 @patch.object(boto3, 'client')
 def test_upload_success_call(mock_client):
@@ -37,3 +39,38 @@ def test_upload_failed(mock_client):
         mock_client.assert_called_with('s3')
 
         storage.upload(key, fake_data)
+
+
+@patch.object(boto3, 'client')
+def test_get_success_call(mock_client):
+    s3_client_mock = MagicMock()
+    mock_client.return_value = s3_client_mock
+    s3_client_mock.get_object.return_value = {
+        'Body': MockS3Body(read=MagicMock(return_value=b'test_data'))
+    }
+    bucket_name = 'test_bucket'
+    key = '0987654321'
+
+    res = S3Storage(bucket_name).get(key)
+
+    s3_client_mock.get_object.assert_called_with(
+        Key=key,
+        Bucket=bucket_name
+    )
+    mock_client.assert_called_with('s3')
+    assert 'test_data' == res
+
+
+@patch.object(boto3, 'client')
+def test_get_failed(mock_client):
+    with pytest.raises(S3StorageError):
+        s3_client_mock = MagicMock()
+        s3_client_mock.get_object = Mock(side_effect=ClientError({}, ''))
+        mock_client.return_value = s3_client_mock
+        bucket_name = 'test_bucket'
+        key = '0987654321'
+
+        storage = S3Storage(bucket_name)
+        mock_client.assert_called_with('s3')
+
+        storage.get(key)
--- tests/mocks.py
+++ tests/mocks.py
@@ -1,5 +1,10 @@
 from typing import NamedTuple
+from unittest.mock import MagicMock
 
 
 class MockResponse(NamedTuple):
     text: str
+
+
+class MockS3Body(NamedTuple):
+    read: MagicMock
